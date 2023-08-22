7f8867ca41aa1a795ff4fa9056446cbf3dc5f32f
BAIDUN-SHE <517870514@qq.com>
2023-08-22 15:31:35 +0800

    增加代码。

--- HardDiskIntegerPartitionCalculator/MainWindow.xaml.cs
+++ HardDiskIntegerPartitionCalculator/MainWindow.xaml.cs
@@ -1,24 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using PropertyChanged;
 
 namespace HardDiskIntegerPartitionCalculator;
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
+
+[AddINotifyPropertyChangedInterface]
 public partial class MainWindow : Window
 {
+    private const double _cylinder = 7.84423828125;
+
+    public int InputPartition { get; set; } = 1;
+
+    public int OutputFAT32 => ((InputPartition - 1) * 4) + (1024 * InputPartition);
+
+    public int OutputNTFS => (int)Math.Ceiling(((int)Math.Ceiling(InputPartition * 1024 / _cylinder)) * _cylinder);
+
     public MainWindow()
     {
         InitializeComponent();
