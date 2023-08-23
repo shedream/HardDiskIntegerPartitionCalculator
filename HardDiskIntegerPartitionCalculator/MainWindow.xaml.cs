c9fcc378e5da8ca6657aa057b4710d863719b214
BAIDUN-SHE <517870514@qq.com>
2023-08-23 14:19:48 +0800

    发布 v1.0版本。

--- HardDiskIntegerPartitionCalculator/MainWindow.xaml.cs
+++ HardDiskIntegerPartitionCalculator/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 using PropertyChanged;
@@ -9,12 +11,42 @@ namespace HardDiskIntegerPartitionCalculator;
 public partial class MainWindow : Window
 {
     private const double _cylinder = 7.84423828125;
+    private string _inputPartitionStr = String.Empty;
 
-    public int InputPartition { get; set; } = 1;
+    public static Version? Version => Assembly.GetExecutingAssembly().GetName().Version;
 
-    public int OutputFAT32 => ((InputPartition - 1) * 4) + (1024 * InputPartition);
+    public static string WindowTitle => $"HDIPC：磁盘整数分区计算器 v{Version?.Major}.{Version?.Minor} by SHE";
 
-    public int OutputNTFS => (int)Math.Ceiling(((int)Math.Ceiling(InputPartition * 1024 / _cylinder)) * _cylinder);
+    public string About => $"👨‍💻Based on C#.NET 7.0, Released on {ReleasedLongTime}";
+
+    public int InputPartition { get; set; }
+
+    public string InputPartitionStr
+    {
+        get => _inputPartitionStr;
+        set
+        {
+            value = value.Trim().TrimStart('0');
+
+            if (String.IsNullOrEmpty(value))
+            {
+                _inputPartitionStr = value;
+
+                InputPartition = 0;
+            }
+
+            if (Int32.TryParse(value, out int result) && result >= 0)
+            {
+                _inputPartitionStr = value;
+
+                InputPartition = result;
+            }
+        }
+    }
+
+    public int OutputFAT32 => Math.Max(0, ((InputPartition - 1) * 4) + (1024 * InputPartition));
+    public int OutputNTFS => Math.Max(0, (int)Math.Ceiling(((int)Math.Ceiling(InputPartition * 1024 / _cylinder)) * _cylinder));
+    public string ReleasedLongTime => File.GetLastWriteTime(GetType().Assembly.Location).ToString("yyyy/MM/dd dddd tt HH:mm:ss.fff");
 
     public MainWindow()
     {
